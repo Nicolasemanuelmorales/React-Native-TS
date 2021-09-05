148f738e48e466c9e9d1cbc9d133d4b7a8025545
Nicolás Emanuel Morales <nicolasemanuelmorales@hotmail.com>
2021-09-05 00:53:45 -0300

    simplificacion de validacion

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -42,72 +42,68 @@ export default function Home() {
       child={
         <>
           {imageRandom === undefined ? null : (
-            <Image style={styles.img} source={{ uri: imageRandom.url }} />
-          )}
-          <View style={styles.boxButton}>
-            <Button
-              color={colors.BLANCO}
-              contentStyle={styles.button}
-              icon={() => (
-                <Icon
-                  name={"redo-alt"}
-                  size={16}
-                  type="solid"
+            <>
+              <Image style={styles.img} source={{ uri: imageRandom.url }} />
+              <View style={styles.boxButton}>
+                <Button
                   color={colors.BLANCO}
+                  contentStyle={styles.button}
+                  icon={() => (
+                    <Icon
+                      name={"redo-alt"}
+                      size={16}
+                      type="solid"
+                      color={colors.BLANCO}
+                    />
+                  )}
+                  onPress={getCat}
+                  children={"Recargar"}
                 />
-              )}
-              onPress={getCat}
-              children={"Recargar"}
-            />
 
-            <Button
-              color={colors.BLANCO}
-              disabled={
-                imageRandom === undefined
-                  ? null
-                  : favoritos.filter((item) => item === imageRandom.url)
-                      .length === 0
-                  ? false
-                  : true
-              }
-              contentStyle={{
-                backgroundColor:
-                  imageRandom === undefined
-                    ? null
-                    : favoritos.filter((item) => item === imageRandom.url)
-                        .length === 0
-                    ? colors.ROJO_PRINCIPAL
-                    : colors.ROJO_PRINCIPAL_DISABLE,
-              }}
-              icon={() => (
-                <Icon
-                  name={"star"}
-                  size={16}
-                  type="solid"
+                <Button
                   color={colors.BLANCO}
+                  disabled={
+                    favoritos.filter((item) => item === imageRandom.url)
+                      .length === 0
+                      ? false
+                      : true
+                  }
+                  contentStyle={{
+                    backgroundColor:
+                      favoritos.filter((item) => item === imageRandom.url)
+                        .length === 0
+                        ? colors.ROJO_PRINCIPAL
+                        : colors.ROJO_PRINCIPAL_DISABLE,
+                  }}
+                  icon={() => (
+                    <Icon
+                      name={"star"}
+                      size={16}
+                      type="solid"
+                      color={colors.BLANCO}
+                    />
+                  )}
+                  onPress={() => {
+                    setAlert(true),
+                      dispatch(favoritoAction([...favoritos, imageRandom.url]));
+                  }}
+                  children={"Favorito"}
                 />
-              )}
-              onPress={() => {
-                setAlert(true),
-                  imageRandom === undefined
-                    ? null
-                    : dispatch(favoritoAction([...favoritos, imageRandom.url]));
-              }}
-              children={"Favorito"}
-            />
-          </View>
-          <Snackbar
-            wrapperStyle={{ alignSelf: "center" }}
-            style={styles.alert}
-            visible={alert}
-            onDismiss={() => setAlert(false)}
-            duration={1000}
-            action={{
-              label: "X",
-            }}
-            theme={{ colors: { accent: colors.BLANCO } }}
-            children={"Se agrego la imagen a favoritos!"}
-          />
+              </View>
+              <Snackbar
+                wrapperStyle={{ alignSelf: "center" }}
+                style={styles.alert}
+                visible={alert}
+                onDismiss={() => setAlert(false)}
+                duration={1000}
+                action={{
+                  label: "X",
+                }}
+                theme={{ colors: { accent: colors.BLANCO } }}
+                children={"Se agrego la imagen a favoritos!"}
+              />
+            </>
+          )}
           <Modal isVisible={loader} backdropOpacity={0.6} animationIn="flash">
             {loader ? (
               <Loader size={50} color={colors.ROJO_PRINCIPAL} />
